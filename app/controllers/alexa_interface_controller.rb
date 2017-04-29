7e26d4b36b4819d1731ae79edccbaac2c77e7e73
Brianna Forster & Ransom Byers <pair+b-forster.rasnom@devbootcamp.com>
2017-04-28 21:34:30 -0700

    Give Alexa something to respond with

--- app/controllers/alexa_interface_controller.rb
+++ app/controllers/alexa_interface_controller.rb
@@ -1,7 +1,11 @@
 class AlexaInterfaceController < ApplicationController
   def recommend
     respond_to do |f|
-      f.json {render json: {stuff: "things"}}
+      f.json {
+        response = AlexaRubykit::Response.new
+        response.add_speech('Clearly not a date with James because you did not make the date James app')
+        render json: response.build_response
+      }
     end
   end
 end

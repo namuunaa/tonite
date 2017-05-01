918c344ad42aa7b68f571f9aeead8a838c96502d
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-05-01 11:48:32 -0700

    Add catch for no intent in request and fix syntax error in creating new request with AlexaRubykit

--- app/controllers/alexa_interface_controller.rb
+++ app/controllers/alexa_interface_controller.rb
@@ -2,14 +2,18 @@ class AlexaInterfaceController < ApplicationController
   def recommend
     respond_to do |f|
       f.json {
-        case params["request"]["intent"]["name"]
+        if params["request"]["intent"]
+          case params["request"]["intent"]["name"]
 
-        when "WingItIntent"
+          when "WingItIntent"
+            render json: create_response(get_location)
+          when "SetAddressIntent"
+            response = AlexaRubykit::Response.new()
+            response.add_speech("hit address intent")
+            render json: response.build_response
+          end
+        else
           render json: create_response(get_location)
-        when "SetAddressIntent"
-          response = AlexaRubykit.new()
-          response.add_speech("hit address intent")
-          render json: response.build_response
         end
       }
     end

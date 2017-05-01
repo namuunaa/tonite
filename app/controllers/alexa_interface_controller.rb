c1bd1dd3dd85865015fc98775c04bc833bce9ac7
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-05-01 11:43:48 -0700

    Add basic logic in controller to accept different types of requests and a test response to check our progress

--- app/controllers/alexa_interface_controller.rb
+++ app/controllers/alexa_interface_controller.rb
@@ -2,7 +2,15 @@ class AlexaInterfaceController < ApplicationController
   def recommend
     respond_to do |f|
       f.json {
-        render json: create_response(get_location)
+        case params["request"]["intent"]["name"]
+
+        when "WingItIntent"
+          render json: create_response(get_location)
+        when "SetAddressIntent"
+          response = AlexaRubykit.new()
+          response.add_speech("hit address intent")
+          render json: response.build_response
+        end
       }
     end
   end

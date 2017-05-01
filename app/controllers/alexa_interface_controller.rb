56d0905c7378dabae8088aa641548d8bbdbbf02d
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-04-30 17:20:50 -0700

    Add p params to try to examine the object that alexa sends us

--- app/controllers/alexa_interface_controller.rb
+++ app/controllers/alexa_interface_controller.rb
@@ -2,6 +2,7 @@ class AlexaInterfaceController < ApplicationController
   def recommend
     respond_to do |f|
       f.json {
+        p params
         render json: create_response({})
       }
     end

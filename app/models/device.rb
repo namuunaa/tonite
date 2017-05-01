7597e6dd9d2053f0e3af4158f8546e7e84b22cd4
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-05-01 12:14:27 -0700

    Change device model to user model

--- app/models/device.rb
+++
@@ -1,4 +0,0 @@
-class Device < ApplicationRecord
-  validates :device_id, presence: true
-  validates :city, presence: true
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170501180342) do
+ActiveRecord::Schema.define(version: 20170501191145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "devices", force: :cascade do |t|
-    t.text     "device_id"
+  create_table "users", force: :cascade do |t|
+    t.text     "user_id"
     t.string   "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

8680f61a766c9fea45dfd05b13c6419b6386cd60
Brianna Forster <brianna.forster@gmail.com>
2017-05-02 14:06:33 -0700

    Write and pass all tests for filtering out all-day events when the current time is 6 pm or later

--- spec/helpers/alexa_interface_helper_spec.rb
+++ spec/helpers/alexa_interface_helper_spec.rb
@@ -21,19 +21,49 @@ describe AlexaInterfaceHelper do
   end
 
   describe '#select_not_started' do
-    let(:response) do
+    let(:api_response) do
       select_not_started(helper.call)
     end
 
+    let(:all_day_test_response) do
+      events = [{'title' => 'All Day 0 Event', 'venue_name' => 'DBC', 'start_time' => '2017-04-29 18:00', 'olson_path' => 'America/Los_Angeles', 'all_day' => "1"},
+      {'title' => 'All Day 1 Event', 'venue_name' => 'DBC', 'start_time' => '2017-04-29 18:00', 'olson_path' => 'America/Los_Angeles', 'all_day' => "0"},
+      {'title' => 'Hamilton', 'venue_name' => 'DBC', 'start_time' => '2017-04-29 18:00', 'olson_path' => 'America/Los_Angeles', 'all_day' => "1"},
+      {'title' => 'All Day 2 Event', 'venue_name' => 'DBC', 'start_time' => '2017-04-29 18:00', 'olson_path' => 'America/Los_Angeles', 'all_day' => "2"}]
+
+      select_not_started(events)
+    end
+
+
     it 'only selects events whose start time is after the current time' do
-      response.each do |event|
+      api_response.each do |event|
         event_start = Time.parse(event["start_time"])
-        event_not_started = (event_start > Time.now || event['all_day'] != 0 )
+        event_not_started = (event_start > Time.now || event['all_day'] != "0" )
         expect(event_not_started).to be true
       end
     end
 
+    it 'selects all-day events if the current time is after 6 pm' do
+      current_time = Time.parse(Time.now.strftime('%F') + " 17:59:00")
 
+      allow(Time).to receive(:now).and_return(current_time)
+
+      all_day_test_response.each do |event|
+
+        expect(all_day_test_response.length).to be 3
+      end
+    end
+
+    it 'does not select all-day events if the current time is 6pm or later' do
+      current_time = Time.parse(Time.now.strftime('%F') + " 18:00:00")
+
+      allow(Time).to receive(:now).and_return(current_time)
+
+      all_day_test_response.each do |event|
+          not_all_day =  event['all_day'] != "0"
+          expect(not_all_day).to be true
+      end
+    end
   end
 
   describe '#pick10' do

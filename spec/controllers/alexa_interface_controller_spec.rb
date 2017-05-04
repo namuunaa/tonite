3ac0af405b350da4d83a712254e00cd11e6461b0
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-05-04 14:45:22 -0700

    Add currently empty tests to test the responses to different intents

--- spec/controllers/alexa_interface_controller_spec.rb
+++ spec/controllers/alexa_interface_controller_spec.rb
@@ -7,8 +7,21 @@ RSpec.describe AlexaInterfaceController, :type => :controller do
                'request' => { 'intent' => { 'name' => 'SetCityIntent', 'slots' => { 'city' => { 'value' => "City" } } } },
                "session" => { 'user' => { 'userId' => "userId"} } }
       post :recommend, json
-      #expect(response).to
+      ap response
+      expect(response).to be(1)
     end
+
+    it 'responds with a default if there is no intent given'
+
+    it 'responds with default if WingItIntent is the intent'
+
+    it 'responds with general help response if AMAZON.HelpIntent is the intent'
+
+    it 'responds by setting the category if SetCategryIntent is the intent'
+
+    it 'responds with category_help_response if CategoryHelpIntent is the intent'
+
+    it 'responds with the city_help_response if CityHelpIntent is the intent'
 
     it 'will set a user location when intent is SetCityIntent' do
       json = { 'format' => 'json',

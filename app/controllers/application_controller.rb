25763b7dc76123008b60ea351b561f35995db53e
Brianna Forster <brianna.forster@gmail.com>
2017-05-04 18:06:39 -0700

    Include AlexaEastereggHelper in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   include AlexaInterfaceHelper
   include AlexaHelpIntentHelper
+  include AlexaEastereggHelper
 end
